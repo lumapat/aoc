35b267420b56ff0e6d185f730bcf22504528600c
Miguel Lumapat <lamppot@protonmail.com>
2020-12-06 22:31:13 -0500

    Solve day 2

--- src/Aoc/Input.hs
+++ src/Aoc/Input.hs
@@ -1,25 +1,48 @@
 module Aoc.Input
     ( AocInput (..)
+    , Password (..)
     , parseAocInput
     ) where
 
+import qualified Data.Text as T
 import Data.Text.Lazy
 import Data.Attoparsec.Text.Lazy as L
 
+data Password = Password (Int, Int) Char String
+                    deriving (Eq, Show)
+
 -- TODO: Change invalid to something else?
 data AocInput = Aoc20D1 [Int]
+              | Aoc20D2 [Password]
               | Invalid
 
 justInts :: Parser [Int]
-justInts = L.many' numbers where
+justInts = L.many1' numbers where
     numbers = do
         d <- L.decimal
         L.endOfLine
         return $ d
 
+passwordParser :: Parser [Password]
+passwordParser = L.many1' passwords where
+    passwords = do
+        lowerBound <- L.decimal
+        char '-'
+        upperBound <- L.decimal
+        space
+        c <- anyChar
+        char ':'
+        space
+        p <- L.takeTill (L.isEndOfLine)
+        L.endOfLine
+        return $ Password (lowerBound, upperBound) c (T.unpack p)
+
 aocParser :: Int -> Int -> Parser AocInput
 aocParser 2020 1 = do ints <- justInts
                       return $ Aoc20D1 ints
+
+aocParser 2020 2 = do passwords <- passwordParser
+                      return $ Aoc20D2 passwords
 
 aocParser _ _ = return $ Invalid
 
--- src/Aoc/Solver.hs
+++ src/Aoc/Solver.hs
@@ -1,6 +1,7 @@
 module Aoc.Solver where
 
 import Aoc.Input as AI
+import Data.Ix (inRange)
 import Data.List
 
 solve :: AI.AocInput -> Int -> String
@@ -15,5 +16,16 @@ solve (AI.Aoc20D1 ints) 2 = case head candidates of
         _ -> "This input doesn't work at all"
     where
         candidates = filter ((2020==) . sum) $ filter ((3==) . length . nub) $ mapM (const ints) [1..3]
+
+solve (AI.Aoc20D2 passwords) 1 = show $ length $ filter validPassword passwords
+    where
+        validPassword (AI.Password range c p) = inRange range (length $ filter (==c) p)
+
+solve (AI.Aoc20D2 passwords) 2 = show $ length $ filter validPassword passwords
+    where
+        validPassword (AI.Password (i, i') c p) = lower /= upper
+            where
+                lower = (p !! (i-1)) == c
+                upper = (p !! (i'-1)) == c
 
 solve _ _ = "Invalid input sire!"
