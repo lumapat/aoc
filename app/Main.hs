196222b663147d6d8abf373f509babc12cd9307e
Miguel Lumapat <lamppot@protonmail.com>
2020-12-06 19:20:35 -0500

    Add path arguments

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,14 @@
 module Main where
 
+import Control.Applicative (some)
 import Options.Applicative
 import Data.Semigroup ((<>))
 
 data AocOptions = AocOptions
     { aocYear:: Int
     , aocDay :: Int
+    , aocP1InputPath :: Maybe String
+    , aocP2InputPath :: Maybe String
     }
 
 defaultYear :: Int
@@ -19,20 +22,34 @@ aocOptions = AocOptions
          <> metavar "YEAR"
          <> showDefault
          <> value defaultYear
-         <> help "The year of AoC to run" )
+         <> help "The year of AoC to run"
+          )
       <*> option auto
           ( long "day"
          <> short 'd'
          <> metavar "DAY"
-         <> help "The day to run input with" )
+         <> help "The day to run input with"
+          )
+      <*> optional (strOption
+          ( long "p1"
+         <> short '1'
+         <> metavar "PATH"
+         <> help "Path for input to part one"
+         ))
+      <*> optional (strOption
+          ( long "p2"
+         <> short '2'
+         <> metavar "PATH"
+         <> help "Path for input to part two"
+         ))
 
 main :: IO ()
-main = doThings =<< execParser opts
+main = solve =<< execParser opts
     where opts = info (aocOptions <**> helper)
                ( fullDesc
               <> progDesc "Solve AoC questions"
               <> header "Advent of Code (AoC) solver" )
 
 
-doThings :: AocOptions -> IO ()
-doThings (AocOptions year day) = print ("Hello!" ++ show year ++ " " ++ show day)
+solve :: AocOptions -> IO ()
+solve (AocOptions year day _ _) = print ("Hello!" ++ show year ++ " " ++ show day)
